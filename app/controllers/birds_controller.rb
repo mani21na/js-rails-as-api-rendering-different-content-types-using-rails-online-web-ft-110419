ebf1f6a831ba3af3822e31f06acd23980afab711
Hanna Lee <mani21na@gmail.com>
2020-03-12 04:04:03 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,6 @@
 class BirdsController < ApplicationController
   def index
     @birds = Bird.all
-    render json: { message: 'Hashes of data will get converted to JSON' }
+    render json: @birds
   end
 end
